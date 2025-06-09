a9bd5f42a68e3e286bd090ff216d44f7391de5ae
amanpand3y <aman.20234186@mnnit.ac.in>
2025-06-09 20:42:12 +0530

    utils added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import dotenv from 'dotenv';
 dotenv.config({ path: "./.env" });
 
-
 import connectDB from "./db/index.js";
+import {app} from "./app.js";
 
-connectDB();
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at: ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB connection Failed in index.js!!!",err);
+})
 
 
 
